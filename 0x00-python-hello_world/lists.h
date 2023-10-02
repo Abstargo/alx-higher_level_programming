f0ebf2ef0adb29ea1ecbd3507c5d32d879c2411d
Yassir <james.iharo@gmail.com>
2023-10-02 17:23:26 +0100

    Linked list cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,11 +7,10 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * 
+ *
  * Description: singly linked list node structure
- * for Holberton project
-*/
-
+ * 
+ */
 typedef struct listint_s
 {
     int n;
@@ -23,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */

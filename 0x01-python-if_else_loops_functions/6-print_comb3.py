251e69b0d962f4ac5fc1698efd8f5aba383cffa6
Yassir <james.iharo@gmail.com>
2023-10-04 08:14:09 +0100

    00...99

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-output = []
+output = ""
 
 for digit1 in range(0, 9):
     for digit2 in range(digit1 + 1, 10):
-        output.append("{:02d}".format(digit1 * 10 + digit2))
+        output += "{:02d}".format(digit1 * 10 + digit2)
+        if digit1 != 8 or digit2 != 9:
+            output += ", "
 
-print(", ".join(output))
+print(output)

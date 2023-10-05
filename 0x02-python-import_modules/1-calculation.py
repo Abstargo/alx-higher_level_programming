6a077a1952fa6b6d1f53ee0981aeadaa85de0408
Yassir <james.iharo@gmail.com>
2023-10-05 15:08:59 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     """ Second thing is to import the function from the 
     other file called calculator_1.py """
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
 

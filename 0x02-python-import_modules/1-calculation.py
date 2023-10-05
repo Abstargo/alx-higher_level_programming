453eb28918ba69d91c57144ffab2307e0abcd43f
Yassir <james.iharo@gmail.com>
2023-10-05 14:59:00 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
 # First difining the variables a and b
-a = 10
-b = 5
+    a = 10
+    b = 5
 
 """ Second thing is to import the function from the 
-other file called calculator_1.py """
+    other file called calculator_1.py """
+
 from calculator_1 import add, sub, mul, div
 
 # Next, we store the functions values into different variables

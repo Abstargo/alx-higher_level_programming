64be7824c19ad926fe8eb38e12dc52939389d500
Yassir <james.iharo@gmail.com>
2023-10-05 15:02:53 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    # First difining the variables a and b
+
+    """ Second thing is to import the function from the 
+    other file called calculator_1.py """
+
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
+    # Next, we store the functions values into different variables
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    #Last thing is to print the results using the format function by an (:d) integer
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))

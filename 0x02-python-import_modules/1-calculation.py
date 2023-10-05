2970fe48d1b1198bffaf6fb9e1e605764b483a1c
Yassir <james.iharo@gmail.com>
2023-10-05 15:00:27 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,14 +2,14 @@
 
 if __name__ == "__main__":
 # First difining the variables a and b
-    a = 10
-    b = 5
 
 """ Second thing is to import the function from the 
     other file called calculator_1.py """
 
 from calculator_1 import add, sub, mul, div
 
+a = 10
+b = 5
 # Next, we store the functions values into different variables
 result_add = add(a, b)
 result_sub = sub(a, b)

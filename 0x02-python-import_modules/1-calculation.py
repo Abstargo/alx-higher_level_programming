6ef6b26ff75edcc6ef618c5596af3e45f40b013d
Yassir <james.iharo@gmail.com>
2023-10-05 14:34:27 +0100

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# First difining the variables a and b
+a = 10
+b = 5
+
+""" Second thing is to import the function from the 
+other file called calculator_1.py """
+from calculator_1 import add, sub, mul, div
+
+# Next, we store the functions values into different variables
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+#Last thing is to print the results using the format function by an (:d) integer
+print("{:d} + {:d} = {:d}".format(a, b, result_add))
+print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+print("{:d} / {:d} = {:.0f}".format(a, b, result_div))

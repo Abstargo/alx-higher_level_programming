0bc94f5116f904c86b9b6f002f66b7a03aa927bf
Yassir <james.iharo@gmail.com>
2023-10-07 14:43:34 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,4 @@ def multiple_returns(sentence):
     """A function that returns a tuple of its length"""
     if sentence == "":
         return (0, None)
-    return(len(sentence), sentence[0])
+    return (len(sentence), sentence[0])

27dd565302b0d2bf108ede9de3293f16fba05378
Yassir <james.iharo@gmail.com>
2023-10-29 19:41:20 +0100

    Intergers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Define a funtion"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Prototype: def add_integer(a, b=98):
+    a and b must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+
+    Args:
+        a (int or float): The first number.
+        b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)

3e9bd607db8769e75e46798cc56d3c8e84e611e0
Yassir <james.iharo@gmail.com>
2023-11-01 19:54:44 +0100

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Define a class"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of a rectangle.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        pass

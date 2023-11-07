167727a8fcac34f508ee0ac522e1cb7044bad6da
Yassir <james.iharo@gmail.com>
2023-11-07 11:01:22 +0100

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a function"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")

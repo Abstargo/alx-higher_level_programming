5caf262872e00a75db9240b8a91175d3af5b4735
Yassir <james.iharo@gmail.com>
2023-12-16 18:22:12 +0100

    SQL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- lists all privleges of the MySQL users user_0d_1 and user_0d_2
+-- creates the MySQL server user user_0d_1.
+	-- user_0d_1 should have all privileges on your MySQL server.
+	-- password set to user_0d_1_pwd
+	-- If the user user_0d_1 already exists, script should not fall.
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

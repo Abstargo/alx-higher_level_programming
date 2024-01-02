8127fa2474e85d5fb1553bdba999b33ee1a92248
Yassir <james.iharo@gmail.com>
2024-01-02 12:41:29 +0100

    ok

--- 0x0D-SQL_introduction/11-best_score.sql
+++
@@ -1,2 +0,0 @@
--- Lists all records of second_table ordered by score
-SELECT score, name FROM second_table WHERE score >= 10 OREDER BY score DESC;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++
@@ -1,2 +0,0 @@
--- inserts new row into table
-INSERT INTO `firs_table` (`id`, `name`) VALUES (89, 'Best School');

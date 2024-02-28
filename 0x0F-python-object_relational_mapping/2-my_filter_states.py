d257fa42e369de6a4ee1389211ad482465b5f8ed
Yassir <james.iharo@gamail.com>
2024-02-28 19:22:16 +0100

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=33306, user=argv[1],
-            passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.

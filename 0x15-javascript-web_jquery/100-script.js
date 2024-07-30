0072ac004affb249a2886fa3d4fedd27f5357f3c
Abstargo <james.iharo@gmail.com>
2024-07-30 14:23:14 +0100

    javascript

--- 0x15-javascript-web_jquery/100-script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Select the <header> element using document.querySelector()
-    var headerElement = document.querySelector('header');
-
-    // Update the text color of the <header> element to red (#FF0000)
-    headerElement.style.color = '#FF0000';
-});

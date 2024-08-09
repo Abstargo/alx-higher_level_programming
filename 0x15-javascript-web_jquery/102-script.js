d602ae132b722642101df5e2f806db2d1398f19c
Abstargo <james.iharo@gmail.com>
2024-08-09 18:48:20 +0100

    jquery API

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,16 +1,26 @@
-$(document).ready(function () {
-	//Event handler for adding a new <li> item
-	$('add_item').click(function() {
-		$('ul.my_list').append('<li>Item</li>');
-	});
+$(document).ready(function() {
+	$('#btn_translate').click(function() {
+		// Get the langauge code entered by the user
+		var languageCode = $('#language_code').val();
 
-	//Event handler for removing the last <li> item
-	$('#removing_item').click(function() {
-		$('ul.my_list li:last-child').remove();
-	});
-
-	//Event handler for clearing all <li> items
-	$('#clear_list').click(function() {
-		$('ul.my_list').empty();
+		//Make an AJAX GET request to fetch the translationj
+		$.ajax({
+			url: 'https://www.fourtonfish.com/hellosalut/hello/',
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				lang: languageCode
+			},
+			success: function(response) {
+				//Display the translation of "Hello" in the DIV#hello
+				$('#hello').text(response.hello);
+			},
+			error: function(xhr, status, error)
+			{
+				//Display an error message if the translation request fails
+				$('#hello').text('Error fetching translation');
+				console.error('Error fetching translation:', error);
+			}
+		});
 	});
 });
